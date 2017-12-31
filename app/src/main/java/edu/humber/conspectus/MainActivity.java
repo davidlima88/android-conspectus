e559577adbcefd25c70a11179799c110af26d2cb
David Lima <david.lima.charry@gmail.com>
2017-12-31 15:06:39 -0500

    New Login activity and Fragment for Concepts

--- app/src/main/java/edu/humber/conspectus/MainActivity.java
+++ app/src/main/java/edu/humber/conspectus/MainActivity.java
@@ -2,18 +2,19 @@ package edu.humber.conspectus;
 
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.view.View;
 import android.support.design.widget.NavigationView;
+import android.support.design.widget.Snackbar;
+import android.support.v4.app.Fragment;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.webkit.WebView;
-import android.webkit.WebViewClient;
+import android.view.View;
+import android.widget.LinearLayout;
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
@@ -43,12 +44,12 @@ public class MainActivity extends AppCompatActivity
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
 
-        WebView webView = (WebView)findViewById(R.id.webview);
+        /*WebView webView = (WebView) findViewById(R.id.webview);
         webView.setWebViewClient(new WebViewClient());
         webView.getSettings().setJavaScriptEnabled(true);
         webView.setVerticalScrollBarEnabled(false);
         webView.setHorizontalScrollBarEnabled(false);
-        webView.loadUrl("http://www.google.com/");
+        webView.loadUrl("http://www.google.com/");*/
     }
 
     @Override
@@ -86,21 +87,44 @@ public class MainActivity extends AppCompatActivity
     @SuppressWarnings("StatementWithEmptyBody")
     @Override
     public boolean onNavigationItemSelected(MenuItem item) {
-        // Handle navigation view item clicks here.
         int id = item.getItemId();
 
+        Fragment fragment;
+        Class fragmentClass;
+
+
+        fragmentClass = ConceptFragment.class;
+
         if (id == R.id.nav_explorer) {
-            // Handle the camera action
+            fragmentClass = ConceptFragment.class;
         } else if (id == R.id.nav_bookmarks) {
-
+            fragmentClass = ConceptFragment.class;
         } else if (id == R.id.nav_categories) {
-
+            fragmentClass = ConceptFragment.class;
         } else if (id == R.id.nav_concepts) {
-
+            fragmentClass = ConceptFragment.class;
         } else if (id == R.id.nav_share) {
-
+            fragmentClass = ConceptFragment.class;
         } else if (id == R.id.nav_send) {
+            fragmentClass = ConceptFragment.class;
+        }
 
+        try {
+            fragment = (Fragment) fragmentClass.newInstance();
+            // Insert the fragment by replacing any existing fragment
+//            FragmentManager fragmentManager = getSupportFragmentManager();
+//            fragmentManager.beginTransaction().replace(R.id.flContent, fragment).commit();
+            LayoutInflater inflater = getLayoutInflater();
+            LinearLayout container = (LinearLayout) findViewById(R.id.flContent);
+            inflater.inflate(R.layout.activity_main, container);
+
+            // Highlight the selected item has been done by NavigationView
+            item.setChecked(true);
+            // Set action bar title
+            setTitle(item.getTitle());
+            // Close the navigation drawer
+        } catch (Exception e) {
+            e.printStackTrace();
         }
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);

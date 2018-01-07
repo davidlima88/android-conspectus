7a536f3f962ef5842fbe7e5ce0dd15a0c00ef4de
David Lima <david.lima.charry@gmail.com>
2018-01-07 12:19:57 -0500

    Entity parsing

--- app/src/main/java/edu/humber/conspectus/adapter/MyEntityRecyclerViewAdapter.java
+++ app/src/main/java/edu/humber/conspectus/adapter/MyEntityRecyclerViewAdapter.java
@@ -33,7 +33,7 @@ public class MyEntityRecyclerViewAdapter extends RecyclerView.Adapter<MyEntityRe
     public void onBindViewHolder(final ViewHolder holder, int position) {
         holder.mItem = mValues.get(position);
         holder.mIdView.setText(mValues.get(position).getId().toString());
-        holder.mContentView.setText(mValues.get(position).getName());
+        holder.mContentView.setText(mValues.get(position).getText());
 
         holder.mView.setOnClickListener(new View.OnClickListener() {
             @Override
--- app/src/main/java/edu/humber/conspectus/model/Entity.java
+++ app/src/main/java/edu/humber/conspectus/model/Entity.java
@@ -9,7 +9,9 @@ import java.util.List;
 
 public class Entity{
     private Integer id;
-    private String name;
+    private String type;
+    private String text;
+    private Double relevance;
     private Integer bookmarks_id;
 
     private Entity() { }
@@ -22,24 +24,47 @@ public class Entity{
         this.id = id;
     }
 
-    public String getName() { return name; }
+    public String getType() {
+        return type;
+    }
 
-    public void setName(String name) { this.name = name; }
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public Double getRelevance() {
+        return relevance;
+    }
+
+    public void setRelevance(Double relevance) {
+        this.relevance = relevance;
+    }
 
     public Integer getBookmarks_id() {
         return bookmarks_id;
-    }
-
-    public void setBookmarks_id(Integer bookmarks_id) {
-        this.bookmarks_id = bookmarks_id;
     }
 
     @Override
     public String toString() {
         return "Entity{" +
                 "id=" + id +
-                ", name=" + name +
+                ", type='" + type + '\'' +
+                ", text='" + text + '\'' +
+                ", relevance=" + relevance +
+                ", bookmarks_id=" + bookmarks_id +
                 '}';
+    }
+
+    public void setBookmarks_id(Integer bookmarks_id) {
+        this.bookmarks_id = bookmarks_id;
     }
 
     public static List<Entity> parseJSONArray(JSONArray jsonArray) throws JSONException {
@@ -47,8 +72,10 @@ public class Entity{
         for (int i = 0; i < jsonArray.length(); i++) {
             JSONObject json_data = jsonArray.getJSONObject(i);
             Entity entity = new Entity();
-            entity.setName(json_data.getString("fish_img"));
-            entity.setId(json_data.getInt("price"));
+            entity.setText(json_data.getString("text"));
+            entity.setType(json_data.getString("type"));
+            entity.setRelevance(json_data.getDouble("relevance"));
+            entity.setId(json_data.getInt("id"));
             data.add(entity);
         }
         return data;

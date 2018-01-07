698c4467b97b61ecc78a2100324233b2764f0f03
SukhpreetBedi <31667962+SukhpreetBedi@users.noreply.github.com>
2018-01-07 14:12:26 -0500

    All models,fragments structure updated

--- app/src/main/java/edu/humber/conspectus/model/Bookmark.java
+++ app/src/main/java/edu/humber/conspectus/model/Bookmark.java
@@ -9,8 +9,14 @@ import java.util.List;
 
 public class Bookmark{
     private Integer id;
-    private String name;
-    private Integer bookmarks_id;
+    private String title;
+    private String url;
+    private Double sentiment;
+    private Double joy;
+    private Double fear;
+    private Double anger;
+    private Double disgust;
+    private Double sadness;
 
     private Bookmark() { }
 
@@ -22,23 +28,82 @@ public class Bookmark{
         this.id = id;
     }
 
-    public String getName() { return name; }
-
-    public void setName(String name) { this.name = name; }
-
-    public Integer getBookmarks_id() {
-        return bookmarks_id;
+    public String getTitle() {
+        return title;
     }
 
-    public void setBookmarks_id(Integer bookmarks_id) {
-        this.bookmarks_id = bookmarks_id;
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public Double getSentiment() {
+        return sentiment;
+    }
+
+    public void setSentiment(Double sentiment) {
+        this.sentiment = sentiment;
+    }
+
+    public Double getJoy() {
+        return joy;
+    }
+
+    public void setJoy(Double joy) {
+        this.joy = joy;
+    }
+
+    public Double getFear() {
+        return fear;
+    }
+
+    public void setFear(Double fear) {
+        this.fear = fear;
+    }
+
+    public Double getAnger() {
+        return anger;
+    }
+
+    public void setAnger(Double anger) {
+        this.anger = anger;
+    }
+
+    public Double getDisgust() {
+        return disgust;
+    }
+
+    public void setDisgust(Double disgust) {
+        this.disgust = disgust;
+    }
+
+    public Double getSadness() {
+        return sadness;
+    }
+
+    public void setSadness(Double sadness) {
+        this.sadness = sadness;
     }
 
     @Override
     public String toString() {
         return "Bookmark{" +
                 "id=" + id +
-                ", name=" + name +
+                ", title='" + title + '\'' +
+                ", url='" + url + '\'' +
+                ", sentiment=" + sentiment +
+                ", joy=" + joy +
+                ", fear=" + fear +
+                ", anger=" + anger +
+                ", disgust=" + disgust +
+                ", sadness=" + sadness +
                 '}';
     }
 
@@ -47,8 +112,15 @@ public class Bookmark{
         for (int i = 0; i < jsonArray.length(); i++) {
             JSONObject json_data = jsonArray.getJSONObject(i);
             Bookmark bookmark = new Bookmark();
-            bookmark.setName(json_data.getString("fish_img"));
-            bookmark.setId(json_data.getInt("price"));
+            bookmark.setId(json_data.getInt("id"));
+            bookmark.setUrl(json_data.getString("url"));
+            bookmark.setTitle(json_data.getString("title"));
+            bookmark.setSentiment(json_data.getDouble("sentiment"));
+            bookmark.setAnger(json_data.getDouble("anger"));
+            bookmark.setDisgust(json_data.getDouble("disgust"));
+            bookmark.setFear(json_data.getDouble("fear"));
+            bookmark.setJoy(json_data.getDouble("joy"));
+            bookmark.setSadness(json_data.getDouble("sadness"));
             data.add(bookmark);
         }
         return data;
